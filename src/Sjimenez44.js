fb3311c824bb1ebbae43e378a6f611bb2ebd2918
Santiago Jimenez <santiji00@gmail.com>
2021-09-04 05:46:50 -0500

    feat: layout structure

--- src/Sjimenez44.js
+++ src/Sjimenez44.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { Layout, Menu } from 'antd';
+const { Header, Content, Footer } = Layout;
 
 function Sjimenez44() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Header className="header">
+        <div className="logo"></div>
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+          <Menu.Item key="1">Projects</Menu.Item>
+          <Menu.Item key="2">Blog</Menu.Item>
+          <Menu.Item key="3">About</Menu.Item>
+        </Menu>
+      </Header>
+      <Content style={{ padding: '0 50px' }}>
+        <Layout className="site-layout-background" style={{ padding: '24px 0' }}>
+          <Content style={{ padding: '0 24px', minHeight: 540 }}>Content</Content>
+        </Layout>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>{`Created with ❤️ by Santiago Jiménez ©2021`}</Footer>
+    </Layout>
   );
 }
 
