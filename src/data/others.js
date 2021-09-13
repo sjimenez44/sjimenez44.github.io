d83967bf98893787a887af8f202d43475fdc1bd0
Santiago Jimenez <santiji00@gmail.com>
2021-09-12 22:52:10 -0500

    feat: new repositorie

--- src/data/others.js
+++ src/data/others.js
@@ -7,6 +7,15 @@ const projects = [
         src: 'https://jjscript.com/wp-content/uploads/2019/09/javascript-banner.png',
         github: 'https://github.com/sjimenez44/30DaysOfJavascript',
         video: null
+    },
+    {
+        title: 'NodeJS server',
+        description: 'Template for NodeJS server with Dockerfile',
+        tag: 'NodeJS',
+        alt: 'NodeJS',
+        src: 'https://www.arsys.es/blog/file/uploads/2017/04/NodeJS.jpg',
+        github: 'https://github.com/sjimenez44/NodeJS_Server',
+        video: null
     }
 ]
 
